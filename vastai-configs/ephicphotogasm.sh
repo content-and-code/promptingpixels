4f72bc92694d42e093c01b4ab94c7ea248859dfc
Shawn <me@shawn.marketing>
2023-11-15 13:13:38 -0500

    Create ephicphotogasm.sh
    Attempting to use this config for the inpainting video.

---
+++ vastai-configs/ephicphotogasm.sh
@@ -0,0 +1,122 @@
+#!/bin/false
+# This file will be sourced in init.sh
+# Namespace functions with provisioning_
+
+# https://raw.githubusercontent.com/ai-dock/stable-diffusion-webui/main/config/provisioning/default.sh
+
+### Edit the following arrays to suit your workflow
+
+DISK_GB_REQUIRED=30
+
+EXTENSIONS=(
+
+)
+
+CHECKPOINT_MODELS=(
+    https://civitai.com/api/download/models/201259 #epicPhotogasm Z - Universal
+    https://civitai.com/api/download/models/201346 #epicPhotogasm Z - Inpainting
+
+)
+
+LORA_MODELS=(
+    #"https://civitai.com/api/download/models/16576"
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+)
+
+ESRGAN_MODELS=(
+    "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth"
+)
+
+CONTROLNET_MODELS=(
+)
+
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_ALLOCATED=$(($DISK_GB_AVAILABLE + $DISK_GB_USED))
+    provisioning_print_header
+    provisioning_get_extensions
+    provisioning_get_models \
+        "/opt/stable-diffusion-webui/models/Stable-diffusion" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/stable-diffusion-webui/models/Lora" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/stable-diffusion-webui/extensions/sd-webui-controlnet/models" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/stable-diffusion-webui/models/VAE" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_models \
+        "/opt/stable-diffusion-webui/models/ESRGAN" \
+        "${ESRGAN_MODELS[@]}"
+    provisioning_print_end
+}
+
+function provisioning_get_extensions() {
+    for repo in "${EXTENSIONS[@]}"; do
+        dir="${repo##*/}"
+        path="/opt/stable-diffusion-webui/extensions/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating extension: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                    micromamba -n webui run ${PIP_INSTALL} -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading extension: %s...\n" "${repo}"
+            git clone "${repo}" "${path}"
+            if [[ -e $requirements ]]; then
+                micromamba -n webui run ${PIP_INSTALL} -r "${requirements}"
+            fi
+        fi
+    done
+}
+
+function provisioning_get_models() {
+    if [[ -z $2 ]]; then return 1; fi
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    if [[ $DISK_GB_ALLOCATED -ge $DISK_GB_REQUIRED ]]; then
+        arr=("$@")
+    else
+        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
+        arr=("$1")
+    fi
+    
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+    if [[ $DISK_GB_ALLOCATED -lt $DISK_GB_REQUIRED ]]; then
+        printf "WARNING: Your allocated disk size (%sGB) is below the recommended %sGB - Some models will not be downloaded\n" "$DISK_GB_ALLOCATED" "$DISK_GB_REQUIRED"
+    fi
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Web UI will start now\n\n"
+}
+
+# Download from $1 URL to $2 file path
+function provisioning_download() {
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+}
+
+provisioning_start
